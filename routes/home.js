51fa7181c69f7a0ba10f5e6bc0f806fe38e161c4
SkyLibrary <eldwinchristian@yahoo.com>
2015-10-27 13:06:23 +0700

    normalization fixed

--- routes/home.js
+++ routes/home.js
@@ -4,14 +4,29 @@
 
 var express = require('express');
 var router = express.Router();
+var Collection = require('../lib/Collection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('home');
 });
-
 router.post('/indexing', function(req, res, next) {
     console.log(req.body);
+
+
+    var collection = new Collection(); 
+	collection.loadDocuments('../file/ADI/adi.all');
+	collection.loadStopWords('../file/stopWord.txt');
+	collection.processData();
+	collection.countIdf();
+	collection.indexing(
+		'what is information science? give definitions where possible.',
+		req.body.TFD,
+		req.body.IDFD,
+		req.body.NormalizationD,
+		req.body.TFQ,
+		req.body.IDFQ,
+		req.body.NormalizationQ);
     res.redirect('/');
 });
 
